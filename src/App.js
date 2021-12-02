b75370ca614df29966ac891f4eeac879ae8a6786
tuongpixta <tuong.nguyendinh@pixta.co.jp>
2021-12-02 17:57:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Screen from "./components/screen/Screen";
+import Dashboard from "./components/dashboard/Dashboard";
+import Todo from "./components/todo/Todo";
 
 function App() {
+  const [token, setToken] = useState(window.sessionStorage.getItem("token"));
+
+  const handleLogout = () => {
+    window.sessionStorage.removeItem("token");
+    setToken("");
+    console.log("Logged out");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="flex justify-between p-5 bg-gray-700">
+        <ul className="">
+          <li className="mr-6">
+            <Link className=" text-white hover:text-gray-300" to="/">
+              Dashboard
+            </Link>
+          </li>
+        </ul>
+        {(token && (
+          <ul className="flex ml-auto">
+            <li className="mr-6">
+              <Link className=" text-white hover:text-gray-300" to="/todo">
+                Todo
+              </Link>
+            </li>
+            <li className="mr-6">
+              <button
+                type="button"
+                className=" text-white hover:text-gray-300"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </li>
+          </ul>
+        )) || (
+          <ul>
+            <li className="mr-6">
+              <Link className=" text-white hover:text-gray-300" to="/login">
+                Login
+              </Link>
+            </li>
+          </ul>
+        )}
+      </nav>
+      <div className="p-5">
+        <Routes>
+          <Route path="/" exact element={<Dashboard />} />
+          <Route path="/todo" exact element={<Todo />} />
+          <Route path="/login" exact element={<Screen />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
